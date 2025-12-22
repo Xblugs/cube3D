b186db2ebc1310f5336746724ec6a1f336dc2e71
cczerwin <cczerwin@student.42lehavre.fr>
2025-12-22 13:49:04 +0100

    Reworked texture loop

--- src/exec/rendering/cube_texturing.c
+++ src/exec/rendering/cube_texturing.c
@@ -34,15 +34,17 @@ void	draw_texture(t_data *data, t_raycast *rc, t_draw *draw)
 	y_max = data->calc->half_height + (rc->wall_dist[rc->ray_index] / 2);
 	while (draw->y[1] != y_max)
 	{
-		predraw_wall(draw, &img, 1);
-		draw_line(data->img, draw);
 		scale_to_ratio(&img, &count, &ratio, &leftover);
+		if (draw->y[1] + floor(ratio) + count >= y_max)
+			predraw_wall(draw, &img, y_max - draw->y[1]);
+		else
+			predraw_wall(draw, &img, ratio + count);
+		draw_line(data->img, draw);
 	}
 }
 
 /*
 	Init img values, separated for norm compliance
-
 */
 static void	prepare_texture(t_data *data, t_img *img, t_draw *d, float *ratio)
 {
@@ -50,7 +52,7 @@ static void	prepare_texture(t_data *data, t_img *img, t_draw *d, float *ratio)
 
 	side = d->color;
 	*ratio = data->rc->wall_dist[data->rc->ray_index] / data->tex->h[side];
-	img->y = 0;
+	img->y = -1;
 	img->x = data->rc->wall_hit[data->rc->ray_index][Y] % UNIT;
 	if (data->rc->ray_status == Y)
 		img->x = data->rc->wall_hit[data->rc->ray_index][X] % UNIT;
@@ -77,21 +79,17 @@ static void	predraw_wall(t_draw *draw, t_img *img, int px_count)
 
 static void	scale_to_ratio(t_img *i, int *count, float *ratio, float *leftover)
 {
-	(*count)++;
-	if (*count >= *ratio)
+	*leftover += *ratio - floor(*ratio);
+	*count = 0;
+	i->y++;
+	if (*leftover >= 1)
 	{
-		*leftover += *ratio - *count;
-		*count = 0;
-		i->y++;
-		if (*leftover >= 1)
-		{
-			(*count)--;
-			*leftover -= 1;
-		}
-		else if (*leftover <= -1)
-		{
-			(*count)++;
-			*leftover += 1;
-		}
+		(*count)++;
+		*leftover -= 1;
+	}
+	else if (*leftover <= -1)
+	{
+		(*count)--;
+		*leftover += 1;
 	}
 }
